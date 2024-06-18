13abe1763e5eec12184ee57a5c721fe896482466
Eben Wagner <ebenwagnernz@gmail.com>
2024-06-18 12:21:38 +1200

    Add project files.

---
+++ Labyrinth/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Labyrinth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TypeWriter("Welcome to my te");
+
+
+
+        }
+
+        static void TypeWriter(string text)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(50);
+            }
+            Console.WriteLine();
+        }
+    }
+}
